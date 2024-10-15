1e0767dddc7145ec66e165f6dd1d072fec838559
rjcontrerasr <156374431+rjcontrerasr@users.noreply.github.com>
2024-10-15 16:37:17 -0400

    changes parenthesis to brackets

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,10 @@
 import streamlit as st
-
 from openai import OpenAI
 
 import os
 
 ### Load your API Key
-my_secret_key = st.secrets('MyOpenAIKey')
+my_secret_key = st.secrets['MyOpenAIKey']
 os.environ["OPENAI_API_KEY"] = my_secret_key
 
 
